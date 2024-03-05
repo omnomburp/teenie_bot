86bf9dd532a801e89ac3788767fc820bce0e7f89
omnomburp <89351913+omnomburp@users.noreply.github.com>
2024-03-06 03:00:33 +0800

    update: handle if the reply is longer than 2000 chars

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 import ollama
+import math
 from discord.ext import commands
 
 intents = discord.Intents.default()
@@ -16,7 +17,16 @@ async def on_ready():
 async def teenie(ctx, *args):
     user_message = " ".join(args)
     bot_response = await send_msg(user_input = user_message)
-    await ctx.send(bot_response)
+    if len(bot_response) > 2000:
+        response_len = len(bot_response)
+        curr_it = 2000
+        count = math.floor(response_len / 2000)
+        await ctx.send(bot_response[:2000])
+        for i in range(count):
+            await ctx.send(bot_response[curr_it:])
+            curr_it = curr_it + 2000
+    else:
+        ctx.send(bot_response)
 
 async def send_msg(user_input) -> str:
     response = ollama.chat(model='dolphin-phi:2.7b-v2.6-q6_K', messages=[
